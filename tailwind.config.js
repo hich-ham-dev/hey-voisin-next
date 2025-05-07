a3a71307bf3a3ae7708aff6f7c31c0aecd8722f2
hich-ham-dev <hicham.hamza.dev@gmail.com>
2025-05-07 22:42:01 +0200

    Add Providers component and integrate HeroUI; update dependencies and page structure

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+import {heroui} from "@heroui/react";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [heroui()]
+}
+
+export default config;
